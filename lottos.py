c4e215562372d2485c3b0127867a6c24cd4d1af7
Dayeon Jeong <jdo0417@ulsan.ac.kr>
2022-01-05 12:21:50 +0900

    .

--- lottos.py
+++ lottos.py
@@ -1,16 +1,5 @@
-def solution(lottos, win_nums):
-    answer = []
-    rank = 7
-    zero = 0
+a, b = map(int, input().strip().split(' '))
+c = '*'
 
-    for i in lottos:
-        rank -= win_nums.count(i)
-    zero = lottos.count(0)
-
-    if rank > 6:
-        rank = 6
-    if zero == 6:
-        zero -= 1
-    answer.append(rank - zero)
-    answer.append(rank)
-    return answer
+c*a
+print(a + b)
